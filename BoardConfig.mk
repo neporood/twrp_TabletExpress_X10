3dadf6ba0f83f3c5f908c59b238c7c68734ff77d
neporood <neporood@live.com>
2016-05-04 17:01:34 -0500

    first commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,20 +2,24 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
+ARCH_ARM_HAVE_NEON := true
 
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/TabletExpress/X10/kernel
+BOARD_KERNEL_CMDLINE := boot_type=0 disp_para=100 fb_base=0x0 config_size=53088 androidboot.serialno=00000000000000000000 androidboot.hardware=sun8i enforcing=1 console=ttyS0,115200 root=/dev/system init=/init vmalloc=384M ion_cma_list=120m,176m,512m loglevel=4 partitions=bootloader@nanda:env@nandb:boot@nandc:system@nandd:misc@nande:recovery@nandf:cache@nandg:metadata@nandh:private@nandi:alog@nandj:UDISK@nandk
 
-BOARD_MKBOOTIMG_ARGS := --base 40000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 01000000 --tags_offset 00000100     
+BOARD_MKBOOTIMG_ARGS := --base 40000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 01000000 --tags_offset 00000100
 
-#MTK common twrp flags
-TW_NO_EXFAT := true
+TARGET_RECOVERY_INITRC := device/TabletExpress/X10/rootdir/init.rc     
+
+# TWRP
 TWHAVE_SELINUX := true
-#TW_USE_TOOLBOX := true
-TW_NO_EXFAT_FUSE := true
-TW_THEME := portrait_mdpi
-#RECOVERY_SDCARD_ON_DATA := true
+TW_THEME := landscape_hdpi
+TWRP_NEW_THEME := false
+TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_REBOOT_RECOVERY := true
+TW_EXCLUDE_SUPERSU := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
--- device_X10.mk
+++ device_X10.mk
@@ -1,12 +1,28 @@
-$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Time Zone data Credits to desalesouche => http://forum.xda-developers.com/showpost.php?p=64850444&postcount=1204
-#PRODUCT_COPY_FILES += \
-#bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL="X10" \
-    TARGET_DEVICE="X10"
-
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_X10
 PRODUCT_DEVICE := X10
+
+# Ramdisk Recovery
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/rootdir/sbin/init_parttion.sh:root/sbin/init_parttion.sh \
+	$(LOCAL_PATH)/rootdir/charger:root/charger \
+	$(LOCAL_PATH)/rootdir/config_mem.ini:root/config_mem.ini \
+	$(LOCAL_PATH)/rootdir/disp.ko:root/disp.ko \
+	$(LOCAL_PATH)/rootdir/file_contexts:root/file_contexts \
+	$(LOCAL_PATH)/rootdir/fstab.sun8i:root/fstab.sun8i \
+	$(LOCAL_PATH)/rootdir/gslX680new.ko:root/gslX680new.ko \
+	$(LOCAL_PATH)/rootdir/gt9xxf_ts.ko:root/gt9xxf_ts.ko \
+	$(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+	$(LOCAL_PATH)/rootdir/init.recovery.sun8i.rc:root/init.recovery.sun8i.rc \
+	$(LOCAL_PATH)/rootdir/initlogo.rle:root/initlogo.rle \
+	$(LOCAL_PATH)/rootdir/nand.ko:root/nand.ko \
+	$(LOCAL_PATH)/rootdir/property_contexts:root/property_contexts \
+	$(LOCAL_PATH)/rootdir/recovery.fstab:root/etc/recovery.fstab \
+	$(LOCAL_PATH)/rootdir/service_contexts:root/service_contexts \
+	$(LOCAL_PATH)/rootdir/sunxi_tr.ko:root/sunxi_tr.ko \
+	$(LOCAL_PATH)/rootdir/sw-device.ko:root/sw-device.ko \
+	$(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc \
+	$(LOCAL_PATH)/rootdir/ueventd.sun8i.rc:root/ueventd.sun8i.rc
+
